5bc3e6e838d9685b36d12089c11d4d5c97449019
Ck <2436182145@qq.com>
2021-08-16 13:28:53 +0800

    Accept

--- 杭电adm/1170.cpp
+++ 杭电adm/1170.cpp
@@ -6,27 +6,28 @@ void reSum(const char C, double A, double  B) {
 	double sum;
 	switch (C)
 	{
-	case '+':sum = A + B;std::cout << sum << std::endl; break;
-	case '*':sum = A * B;std::cout << sum << std::endl; break;
-	case '-':sum = A - B;std::cout << sum << std::endl; break;
-	case '/':if (int(A) % int(B) == 0) sum = A / B;
-			else
+	case '+':sum = A + B;std::cout << int(sum) << std::endl; break;
+	case '*':sum = A * B;std::cout << int(sum) << std::endl; break;
+	case '-':sum = A - B;std::cout << int(sum) << std::endl; break;
+	case '/':if (int(A) % int(B) == 0) {
 		sum = A / B;
-		std::cout << std::setprecision(2) << std::fixed <<sum  << std::endl;break;
-
+		std::cout << int(sum) << std::endl;
+	}
+			else {
+		sum = A / B;
+		std::cout << std::setprecision(2) << std::fixed << sum << std::endl;break;
+	}
 	}
 }
 int main() {
 	int n;
 	while (std::cin >> n) {
 		while (n--) {
-			double  A, B,sum;
+			double  A, B, sum;
 			char C;
 			std::cin >> C;
 			std::cin >> A >> B;
 			reSum(C, A, B);
-			}
-		
-
 		}
 	}
+}
