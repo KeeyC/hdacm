752de1c2911e40b6985dce374987ed20d0228ce2
Ck <2436182145@qq.com>
2021-08-16 16:36:07 +0800

    Accept

--- 杭电adm/2104.cpp
+++ 杭电adm/2104.cpp
@@ -1,26 +1,43 @@
 #include<iostream>
-#include<vector>
-#include<iterator>
 using namespace std;
 int main() {
 	int M, N;
 	while (cin >> M >> N) {
 		if (M == -1 && N == -1) break;//terminal condition
-		vector<int> A;
-		for (int i = 0;i < M;i++) A.push_back(0);
-		int j = 0;
-		while(A[j]!=1) {
-			A[j] = 1;
-			j += N;
-			if (j >= M)j = j - M;
+		while (N) {
+			int temp = N;
+			N = M % N;
+			M = temp;
 		}
-		bool T = true;
-		for (int k = 0;k < M;k++) {
-			if (A[k] == 0) T = false;
-		}
-		if (T) cout << "Yes" << endl;
+		if (M == 1) cout << "YES" << endl;
 		else
 			cout << "POOR Haha" << endl;
 	}
-	
 }
+
+	//最开始用数组来完成，发现走不通，会memory limit exceeded,
+	// 后来在网上查了后发现需要用公约数来做
+	//int M, N; Wrong!!!
+	//while (cin >> M >> N) {
+	//	if (M == -1 && N == -1) break;//terminal condition
+	//	vector<int> A;
+	//	for (int i = 0;i < M;i++) A.push_back(0);
+	//	int j = 0;
+	//	int tongji = 0;
+	//	while (A[j] != 1) {
+	//		A[j] = 1;
+	//		j += N;
+	//		if (j >= M)j = j - M;
+	//		tongji++;
+	//	}
+	//	bool T = true;
+	//	/*for (int k = 0;k < M;k++) {
+	//		if (A[k] == 0) T = false;
+	//	}*/
+	//	if (tongji == M) T = true;
+	//	if (T) cout << "Yes" << endl;
+	//	else
+	//		cout << "POOR Haha" << endl;
+	//}
+	//return 0;
+//}
